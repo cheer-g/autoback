bd963d87bf5ddbfbb0967a22b30857b2e505757b
cheer-g <44105061+cheer-g@users.noreply.github.com>
2018-10-17 21:20:01 +0530

    Update to test2
    Will find c , txt or random files and move them to corresponding folders

--- test2.py
+++ test2.py
@@ -2,10 +2,20 @@ import shutil
 import os
 source='C:/Users/user/Desktop'
 txt='D:/txt'
+c='D:/txt'
+random='D:/random'
 for file in os.listdir(source):
     if file.endswith(".txt"):
         if not os.path.exists(txt):
             os.makedirs(txt)
         shutil.move(os.path.join(source,file),os.path.join(txt,file))
+    elif file.endswith(".c"):
+        if not os.path.exists(c):
+            os.makedirs(c)
+        shutil.move(os.path.join(source,file),os.path.join(c,file))
+    else:
+        if not os.path.exists(random):
+            os.makedirs(random)
+        shutil.move(os.path.join(source,file),os.path.join(random,file))
 print "Operation successful"
     
